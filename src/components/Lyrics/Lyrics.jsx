dee076ac6c6bb74684a236a0c32e77a203db3746
IvanZemsky <i.ivanzemsky2005@gmail.com>
2023-09-24 21:05:32 +0300

    interface 2

--- src/components/Lyrics/Lyrics.jsx
+++ src/components/Lyrics/Lyrics.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import './Lyrics.scss';
 
 function Lyrics() {
   return (
-    <div>Lyrics</div>
+    <div className="lyrics">
+      <p>    
+        Lorem ipsum dolor sit amet<br/>
+        Consectetur adipiscing elit.<br/>
+        Morbi orci mi<br/>
+        scelerisque et <br/>
+        <br/>
+        aliquet at,<br/>
+        fermentum et erat<br/>
+        Proin consectetur egestas sem consequat pellentesque<br/>
+        <br/>
+        Nam id scelerisque dolor<br/>
+        eget venenatis est<br/>
+        <br/>
+        Sed vitae rhoncus nulla<br/>
+        sit amet porttitor magna. Curabitur finibus felis viverra<br/>
+        augue accumsan, ut molestie<br/>
+        <br/>
+        leo gravida. Vivamus vitae sem eget elit porta vulputate eget in metus. Ut hendrerit nibh tortor<br/>
+        ut accumsan dolor rutrum ut<br/>
+        <br/>
+        Sed laoreet pulvinar urna, ac<br/>
+        convallis velit euismod posuere<br/>
+        Donec hendrerit erat ut maximus<br/>
+        dictum. Duis sit amet ultrices<br/>
+        quam<br/>
+        <br/>
+        Nam non tellus
+      </p>
+    </div>
   )
 }
 
